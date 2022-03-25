c75ecb262916182a9c4872116426cb8b9081c989
Hanna Pshanichnaya <hannapshanich@gmail.com>
2022-03-25 18:19:56 +0100

    redux Products

--- src/App.js
+++ src/App.js
@@ -1,30 +1,32 @@
 import React from 'react';
 import Products from './Components/Products';
-import data from './data.json';
+
+import store from './store';
+import { Provider } from 'react-redux';
 
 class App extends React.Component {
   constructor() {
     super();
-    this.state = {
-      products: data.products,
-    };
+    this.state = {};
   }
   render() {
     return (
-      <div className="grid-container">
-        <header>
-          <a href="/">Home Page</a>
-        </header>
-        <main>
-          <div className="content">
-            <div className="main">
-              <Products products={this.state.products} />
+      <Provider store={store}>
+        <div className="grid-container">
+          <header>
+            <a href="/">Home Page</a>
+          </header>
+          <main>
+            <div className="content">
+              <div className="main">
+                <Products products={this.state.products} />
+              </div>
+              <div className="sidebar"></div>
             </div>
-            <div className="sidebar"></div>
-          </div>
-        </main>
-        <footer>All rights reserved</footer>
-      </div>
+          </main>
+          <footer>All rights reserved</footer>
+        </div>
+      </Provider>
     );
   }
 }
