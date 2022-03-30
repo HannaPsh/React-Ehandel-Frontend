c61e8c6caf187d168d977b63abbee73261ac17f9
Hourieh <hoorieh.23@gmail.com>
2022-03-30 14:38:35 +0200

    changes in gitignore

--- src/Components/Signup.js
+++ src/Components/Signup.js
@@ -1,4 +1,7 @@
 import React from 'react'
+import './Loginsignup.css';
+
+
 
 const Signup = () => {
     return (
@@ -15,6 +18,16 @@ const Signup = () => {
       <form>
   <div class="Username">
     <label for="InputUsername">Username</label>
+    <input type="username" class="Username" id="InputUsername" aria-describedby="Userhelp" placeholder="Enter Username"/>
+    
+  </div>
+  <div class="Lastname">
+    <label for="InputLastname">Lastname</label>
+    <input type="Lastname" class="Lastname" id="InputLastname" aria-describedby="Lasthelp" placeholder="Enter Lastname"/>
+    
+  </div>
+  <div class="Email">
+    <label for="InputEmail">Email</label>
     <input type="email" class="Email" id="InputEmail1" aria-describedby="emailHelp" placeholder="Enter email"/>
     <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
   </div>
