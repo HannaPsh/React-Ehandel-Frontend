f477ec8a827edd2ff13b955a252b46391df8e775
apnan <aishajahan2015@gmail.com>
2022-04-02 13:12:03 -1200

    close button udside

--- src/Components/CookieConsent.js
+++ src/Components/CookieConsent.js
@@ -35,6 +35,8 @@ export default class CookieConsent extends Component {
       This site uses cookies. See our <button onClick={this.handleOpenModal}>Privacy policy</button> for more.
       </Consent>
       <Modal isOpen={this.state.showModal}>
+      <button onClick={this.handleCloseModal}>Close</button>
+
       <h2>
   Cookie Policy of ICONIC
 </h2>
@@ -48,7 +50,6 @@ Some of the purposes for which Trackers are used may also require the User's con
 This Website uses Trackers managed directly by the Owner (so-called “first-party” Trackers) and Trackers that enable services provided by a third-party (so-called “third-party” Trackers). Unless otherwise specified within this document, third-party providers may access the Trackers managed by them.
 The validity and expiration periods of Cookies and other similar Trackers may vary depending on the lifetime set by the Owner or the relevant provider. Some of them expire upon termination of the User’s browsing session.
 In addition to what’s specified in the descriptions within each of the categories below, Users may find more precise and updated information regarding lifetime specification as well as any other relevant information – such as the presence of other Trackers - in the linked privacy policies of the respective third-party providers or by contacting the Owner.
-      <button onClick={this.handleCloseModal}>Close</button>
     </Modal>
 
 
