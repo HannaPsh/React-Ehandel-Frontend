a877fef99b5334a130b3af78923c7f0cb23765ef
Bhavani Palla <bhavani.palla@gmail.com>
2022-04-04 17:41:54 +0200

    fixing cart

--- src/Components/cart/CartItems.js
+++ src/Components/cart/CartItems.js
@@ -3,8 +3,6 @@ import CartItem from "./CartItem";
 import "./cart.css";
 import formatCurrency from "../../util";
 
-
-
 const CartItems = ({ cartItems, removeFromCart }) => {
   return (
     <div className="cart">
@@ -14,16 +12,19 @@ const CartItems = ({ cartItems, removeFromCart }) => {
         <div>
           <ul className="cart-items">
             {cartItems.map((item) => (
-              <CartItem item={item} removeFromCart={removeFromCart} />
+              <CartItem
+                item={item}
+                removeFromCart={removeFromCart}
+                key={item._id}
+              />
             ))}
-            </ul>
+          </ul>
 
           <div className="total">
-              <div>
-              <label>Total :</label>
-              {formatCurrency(
-                cartItems.reduce((a, c) => a + c.price * c.count, 0)
-              )}
+            <div>
+              <label>Total : SEK </label>
+              {cartItems.reduce((a, c) => a + c.price * c.count, 0)}
+              
             </div>
           </div>
         </div>
